2afc5a87ccbb3c587c912db483aceb9cc3b9c6b1
RicardoJun10r <ricardojunior0110@gmail.com>
2023-06-28 13:06:32 -0300

    fix: bug in a println function

--- DNN/src/Demo.java
+++ DNN/src/Demo.java
@@ -5,49 +5,51 @@ public class Demo {
 
     public static void main(String[] args) {
         
-        Double[][]treino = {
+        double[][]treino = {
             { 0.0, 0.0, 0.0 },
-            { 1.0, 0.0, 0.0 },
-            { 0.0, 1.0, 0.0 },
+            { 1.0, 0.0, 1.0 },
+            { 0.0, 1.0, 1.0 },
             { 1.0, 1.0, 1.0 },
         };
 
-        Double[] res = new Double[treino.length];
+        double[] res = new double[treino.length];
 
-        Double[] x = new Double[treino[0].length-1];
+        double[] x = new double[treino[0].length-1];
         
         x[0] = random.nextDouble();        
         
         x[1] = random.nextDouble();
 
-        Double b = random.nextDouble();
+        double b = random.nextDouble()+1;
 
-        Double l_rate = 0.25;
+        double l_rate = 0.25;
         
         // W[1x2] * A[2x1] = SOP[1x1]
-        Double erro = 0.0;
+        double erro = 0.0;
         
-        for(int e = 0; e < 2; e++){
+        for(int e = 0; e < 100*1000; e++){
             
             erro = 0.0;
 
             for(int i = 0; i < treino.length; i++){
                 
-                Double y = (treino[i][0]*x[0] + treino[i][1]*x[1]) + b;
-                System.out.println("treino[i][0] = " + treino[i][0] + " * x[0] = " + x[0] + " + treino[i][1] = " + treino[i][1] + " * x[1] = " + x[1] + " + b = " + b);
+                double y = (treino[i][0]*x[0] + treino[i][1]*x[1]) + b;
+
                 res[i] = fsigmoide(y);
-                System.out.println("res["+i+"] = " + res[i]);
-                
+
                 erro += fcusto(res[i], treino[i][2]);
                 
             }
-            System.out.println("ERRO = " + (erro/4) + " w0 = " + x[0] + " w1 = " + x[1]);
-            gradient(res, treino, x, l_rate, b);
+            double [] nw = gradient(res, treino, x, l_rate, b);
+            x[0] = nw[0];
+            x[1] = nw[1];
+            b = nw[2];
         }
 
         for(int i = 0; i < 2; i++){
             for(int j = 0; j < 2; j++){
-                System.out.println(treino[j][0] + " | " + treino[i][0] + " = " + fsigmoide(treino[j][0]*x[0] + treino[i][0]*x[1]) + b);
+                double y = fsigmoide((treino[j][0]*x[0] + treino[i][0]*x[1]) + b);
+                System.out.println(treino[j][0] + " | " + treino[i][0] + " = " + y);
             }
         }
 
@@ -55,38 +57,40 @@ public class Demo {
 
     }
 
-    static Double fcusto(Double predicted, Double desired){
-        Double erro = (predicted - desired);
+    static double fcusto(double predicted, double desired){
+        double erro = (predicted - desired);
         return (erro*erro);
     }
 
-    static Double fsigmoide(Double valor){
+    static double fsigmoide(double valor){
         return(1/(1+Math.exp(-valor)));
     }
 
-    static void gradient(Double[] predicted, Double[][] train, Double[]w, Double lr, Double b){
+    static double[] gradient(double[] predicted, double[][] train, double[]w, double lr, double b){
 
         Integer size = train.length;
 
-        Double dw1 = 0.0, dw2 = 0.0, db = 0.0;
+        double dw1 = 0.0, dw2 = 0.0, db = 0.0;
 
         for(int i = 0; i < size; i++){
-            Double zi = train[i][2];
-            Double di = 2*(predicted[i]-zi)*predicted[i]*(1-predicted[i]);
+            double zi = train[i][2];
+            double di = 2*(predicted[i]-zi)*predicted[i]*(1-predicted[i]);
             dw1 += di*train[i][0];
             dw2 += di*train[i][1];
             db += di;
-            System.out.println("predicted["+i+"] = " + predicted[i] + " - zi = " + zi);
-            System.out.println("train[i][0] = " + train[i][0] + " train[i][1] = " + train[i][1]);
         }
 
         dw1 /= size;
         dw2 /= size;
         db /= size;
 
-        w[0] -= dw1*lr;
-        w[1] -= dw2*lr; 
-        b -= db*lr;
+        double []nw = new double[3];
+
+        nw[0] = w[0] - dw1*lr;
+        nw[1] = w[1] - dw2*lr;
+        nw[2] = b - db*lr;
+
+        return nw;
 
     }
 
